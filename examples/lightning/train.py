827339de32203e6fda096c7223d2ca0db3957db2
tomo <tomo14142356@gmail.com>
2022-04-16 12:03:45 +0900

    Fix multi node train

--- examples/lightning/train.py
+++ examples/lightning/train.py
@@ -11,6 +11,11 @@ from torch.utils.data import DataLoader, Dataset
 from byol_pytorch import BYOL
 import pytorch_lightning as pl
 from pytorch_lightning.loggers import WandbLogger
+# from pytorch_lightning import logging
+
+from utils import dist_setup, dist_cleanup
+from utils import print_rank, myget_rank_size
+from utils import myget_local_rank, myget_node_size
 
 # arguments
 
@@ -35,6 +40,8 @@ parser.add_argument("--result_path",
                     help='path to your folder of result')
 
 args = parser.parse_args()
+
+dist_setup()
 
 # test model, a resnet 50
 
@@ -97,7 +104,8 @@ class ImagesDataset(Dataset):
             if ext.lower() in IMAGE_EXTS:
                 self.paths.append(path)
 
-        print(f'{len(self.paths)} images found')
+        print_rank(f'{len(self.paths)} images found')
+        # print(f'{len(self.paths)} images found')
 
         self.transform = transforms.Compose([
             transforms.Resize(image_size),
@@ -119,11 +127,26 @@ class ImagesDataset(Dataset):
 # main
 
 if __name__ == '__main__':
-    train_ds = ImagesDataset(args.image_folder, IMAGE_SIZE)
-    # train_ds = ImagesDataset(args.image_folder, IMAGE_SIZE, "train")
+    rank, world_size = myget_rank_size()
+    is_cuda = torch.cuda.is_available()
+    local_rank, node_num = myget_local_rank(), myget_node_size()
+    num_workers, ngpus = NUM_WORKERS // node_num, NUM_GPUS
+    if is_cuda:
+        ngpus = torch.cuda.device_count()
+        args.num_gpus = ngpus
+        local_rank = rank % ngpus
+        node_num = world_size // ngpus
+        num_workers = num_workers // node_num
+        # torch.cuda.set_device(local_rank)
+        # device = torch.device("cuda", local_rank)
+
+    print_rank("start main")
+    # print_rank("start main num_workers:", num_workers)
+    # train_ds = ImagesDataset(args.image_folder, IMAGE_SIZE)
+    train_ds = ImagesDataset(args.image_folder, IMAGE_SIZE, "train")
     train_loader = DataLoader(train_ds,
                               batch_size=BATCH_SIZE,
-                              num_workers=NUM_WORKERS,
+                              num_workers=num_workers,
                               shuffle=True)
     # val_ds = ImagesDataset(args.image_folder, IMAGE_SIZE, "val")
     # val_loader = DataLoader(val_ds,
@@ -150,15 +173,22 @@ if __name__ == '__main__':
                          log_model="all",
                          config=args)
 
-    trainer = pl.Trainer(gpus=NUM_GPUS,
-                         max_epochs=EPOCHS,
-                         enable_checkpointing=True,
-                         accumulate_grad_batches=1,
-                         sync_batchnorm=True,
-                         accelerator="gpu",
-                         strategy="ddp",
-                         logger=logger,
-                         log_every_n_steps=1,
-                         default_root_dir=args.result_path)
+    print_rank("start setup train")
+    trainer = pl.Trainer(
+        gpus=ngpus,
+        num_nodes=node_num,
+        # num_processes=ngpus,
+        max_epochs=EPOCHS,
+        enable_checkpointing=True,
+        accumulate_grad_batches=1,
+        sync_batchnorm=True,
+        accelerator="gpu",
+        strategy="ddp",
+        logger=logger,
+        log_every_n_steps=1,
+        default_root_dir=args.result_path)
+    print_rank("start train")
 
     trainer.fit(model, train_loader)
+
+    dist_cleanup()
